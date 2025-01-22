4232c3e5c0baf55a757b099ecc7330a01b9e8ef1
Karla Mendez <karla.mendez.est@tecazuay.edu.ec>
2025-01-21 23:27:03 -0500

    cambios cliente

--- src/app/registroC/registroC.service.ts
+++ src/app/registroC/registroC.service.ts
@@ -10,7 +10,7 @@ import { RegistroC } from './registroC';
 export class RegistroCService {
   private apiUrl = 'http://localhost:8081/api';
   //private apiUrl = 'http://192.168.40.228:8081/api';
-  private urlEndPoint = `${this.apiUrl}/estudiantes`;
+  private urlEndPoint = `${this.apiUrl}/clientes`;
 
   constructor(private http: HttpClient) { }
 
